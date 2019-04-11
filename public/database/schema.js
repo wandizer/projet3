584b4cee1a00c92420cb93892ff8cc2bbe93c301
Andre-DS <dos.santos.andre@outlook.pt>
2019-04-11 18:32:09 +0200

    Mise en place de hotel.db et schema

---
+++ public/database/schema.js
@@ -0,0 +1,13 @@
+export const createTable = {
+  Sercive: '',
+  Role: '',
+  Employe: '',
+  User: '',
+  Room: '',
+  Client: '',
+  Room_Reservation: '',
+  Type_Food: '',
+  Food_Item: '',
+  Stock: '',
+  Appetizers: '',
+};
