0bf9b2fa5d7d5b27ad4bc7d6973e5cb992ac13be
Andre-DS <dos.santos.andre@outlook.pt>
2019-04-19 16:46:29 +0200

    Correction executeQuerys with db.all instead of db.run

--- public/app/connexion/connexion_controller.js
+++ public/app/connexion/connexion_controller.js
@@ -16,10 +16,7 @@ const User = require('./models/User.js');
  * @param password
  */
 function checkCredentials(result, password) {
-  console.log(result);
-  console.log('Inside Callback');
   if (Array.isArray(result) && result.length === 1) {
-    console.log('Il y a un resultat');
     if (password === result[0].password) {
       // redirection to dashboard
       window.location.assign('./views/generic_view.html');
@@ -34,14 +31,12 @@ function checkCredentials(result, password) {
 // When form is submitted
 document.getElementById('login').addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Submitted');
   const formData = new FormData(e.target);
   const username = formData.get('username');
   const password = formData.get('password');
 
   // if form is not empty
   if (username !== '' && password !== '') {
-    console.log('Form not NULL');
     User.getByUsername(username, checkCredentials, password);
   }
 }, false);
